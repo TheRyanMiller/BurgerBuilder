228e8ec121d060491c2dc625d9c47cd5be5edd2c
Ryan <rmiller07@gmail.com>
2018-01-06 10:56:08 -0500

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Auxx from '../../hoc/Auxx';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients:{
+        salad:0,
+        bacon:0,
+        cheese:0,
+        meat:0
+      }
+    };
+  }
+  render () {
+    return (
+      <Auxx>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
+      </Auxx>
+    );
+  }
+}
+
+export default BurgerBuilder;
